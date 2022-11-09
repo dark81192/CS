2460b985c51ec9153d90276e95a83fad897a1fd4
dark81192 <469144759@qq.com>
2022-11-10 01:23:18 +0800

    ver 1.01(update):增加循环

--- CompoundInterestCalculator/CICalc.c
+++ CompoundInterestCalculator/CICalc.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define interest principal * (profitability * 0.01)
 #define pai principal + interest
@@ -15,23 +16,36 @@ int main()
 	char str1[] = "请输入一个固定的年化利率(例：3.3% 输入 3.3)：";
 	char str2[] = "请输入复利周期(单位年)：";
 	char str3[] = "请输入本金：";
+	char loop = 'Y';
 
-	printf("%s", str1);
-	scanf("%f", &profitability);
-	printf("%s", str2);
-	scanf("%d", &year);
-	printf("%s", str3);
-	scanf("%f", &principal);
-
-	while (year >= i)
+	while (loop == 89 || loop == 121)
 	{
-		if(i == 1)
-			printf("利率：%.2f%\n复利执行周期：%d\n本金：%.2f\n\t\n\t\n\t\n", profitability, year, principal);
-		printf("第%d年\n", i);
-		printf("本息合计：%f\n" ,pai);
-		principal = pai;
-		i++;
-	}
+		system("cls");
+		printf("%s", str1);
+		scanf("%f", &profitability);
+		printf("%s", str2);
+		scanf("%d", &year);
+		printf("%s", str3);
+		scanf("%f", &principal);
 
+		while (year >= i)
+		{
+			if (i == 1)
+				printf("利率：%.2f%\n复利执行周期：%d\n本金：%.2f\n\t\n\t\n\t\n", profitability, year, principal);
+			printf("第%d年\n", i);
+			printf("本息合计：%f\n", pai);
+			principal = pai;
+			i++;
+		}
+
+		printf("是否继续(Y/N):");
+		//getchar();
+		rewind(stdin);
+		scanf("%c", &loop);
+	}
+	//if (loop == 'Y' || loop == 'y')
+	//system("cls");
+	exit(0);
+	//goto func;
 	return 0;
 }
