aaf92ab1202ee0068f06848a007051927e82f9d9
dark81192 <469144759@qq.com>
2022-12-03 21:58:40 +0800

    增加新的阶乘功能

--- test_002/test_002.c
+++ test_002/test_002.c
@@ -6,18 +6,57 @@
 int main()
 {
 	//计算n的阶乘
-	int n, s, i, j;
-
-	while (scanf("%d", &n) != EOF)
+	int n, s, i, j, t;
+	printf("1.计算1!+2!+3!+.....+10!\n");
+	printf("2.计算n的阶乘\n输入数字:>");
+	while (scanf("%d", &t) != EOF)
 	{
-		for (i = 1, s = 1; i <= n; i++)
+		///*int ch = 0;
+		//scanf("%d", &ch);*/
+		switch (t)
 		{
-			s *= i;
+		case 1:												//计算1!+2!+3!+.....+10!
+			n = 1;
+			s = 0;
+			while (n <= 10)
+			{
+				for (i = 1, t = 1; i <= n; i++)
+				{
+					t *= i;
+				}
+				n++;
+				s += t;
+			}
+			printf("1!+2!+3!+.....+10!=%d\n", s);
+			system("pause");
+			system("cls");
+			printf("1.计算1!+2!+3!+.....+10!\n");
+			printf("2.计算n的阶乘\n");
+			printf("输入数字:>");
+			break;
+		case 2:												//计算n的阶乘
+			system("cls");
+			printf("输入一个数字计算它的阶乘:>");
+			while (scanf("%d", &n) != EOF)
+			{
+				for (i = 1, t = 1; i <= n; i++)
+				{
+					t *= i;
+				}
+				printf("%d\n", t);
+				system("pause");
+				system("cls");
+				printf("Ctrl + C or Ctrl + Z结束\n输入一个数字计算它的阶乘:>");
+			}
+			system("pause");
+			system("cls");
+			printf("Ctrl + C or Ctrl + Z结束\n");
+			printf("1.计算1!+2!+3!+.....+10!\n");
+			printf("2.计算n的阶乘\n输入数字:>");
+			break;
+		default:
+			break;
 		}
-		printf("%d\n", s);
-		system("pause");
-		system("cls");
 	}
-
 	return 0;
 }
