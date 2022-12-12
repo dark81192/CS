067d165381e94492c0f6b17060fe79fa09fd6e6d
dark81192 <469144759@qq.com>
2022-12-12 21:16:52 +0800

    Binary Search

---
+++ BinarySearch/binarySearch.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	int search = 6;	//需要查找的数字
+	int left = 0;	//左下标
+	int right = len - 1;
+	int mid;
+	
+	for (mid = (left + right) / 2; 1; mid = (left + right) / 2)
+	{
+		if (arr[mid] > search)
+			right = mid;
+		if (arr[mid] < search)
+			left = mid + 1;
+		if (arr[mid] == search)
+		{ 
+			printf("%d 的下标是: %d\n", search, mid);
+			break;
+		}
+		len--;
+		if (len == 0)
+		{
+			printf("没有需要查找的值");
+			break;
+		}
+	}
+	
+	return 0;
+}
