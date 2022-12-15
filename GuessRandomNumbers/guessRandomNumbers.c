7a6374e66a247d992d486a0069e4424a8100439f
dark81192 <469144759@qq.com>
2022-12-15 23:43:57 +0800

    猜数字小游戏

--- GuessRandomNumbers/guessRandomNumbers.c
+++ GuessRandomNumbers/guessRandomNumbers.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
 	int r = rand() %100;
@@ -14,8 +15,12 @@ int main()
 	{
 		scanf("%d", &xnum);
 		if (xnum == r) {
-			printf("恭喜你猜对了！你用了%d次猜对\n输入任意数字再来一次 输入2退出游戏\n", j);
+			printf("恭喜你猜对了！正确数字是%d\n你用了%d次猜对\n输入任意数字再来一次 输入2退出游戏\n", xnum, j);
 			scanf("%d", &i);
+			system("cls");
+			r = rand() % 100;
+			if (i != 2)
+				printf("数字炸弹\n现在来开始游戏吧！\n现在已经有一个1-100的随机数了.\n输入你猜的数字:>");
 		}
 		else if (xnum < r) {
 			printf("这个数字比正确数字小了\n再猜一次:>");
