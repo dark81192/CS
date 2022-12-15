893fc4005250040dfa7ef4bac75d6b91085fbff1
dark81192 <469144759@qq.com>
2022-12-15 23:51:00 +0800

    增加随机数种子

--- GuessRandomNumbers/guessRandomNumbers.c
+++ GuessRandomNumbers/guessRandomNumbers.c
@@ -1,11 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 
 
 int main()
 {
+	srand((unsigned)time(NULL));
 	int r = rand() %100;
 	int xnum = 0, i, j = 1;
 
