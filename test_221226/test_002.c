ab98f2f898cd659080cd6811052ff92c3f96bf26
dark81192 <469144759@qq.com>
2022-12-26 19:52:41 +0800

    取两个数的最大公约数(过程简化)

--- test_221226/test_002.c
+++ test_221226/test_002.c
@@ -4,26 +4,14 @@
 
 void commonDivisor()
 {
-	int a = 0, b = 0;
+	int a = 0, b = 0, i = 0;
 	scanf("%d %d", &a, &b);
-	if (a < b)
+	for (i = a > b ? a : (a == b ? a : b); i > 0; i--)
 	{
-		for (int i = a; i > 0; i--)
-			if (b % i == 0 && a % i == 0)
-			{
-				printf("%d\n", i);
-				break;
-			}
+		if (a % i == 0 && b % i == 0)
+		{
+			printf("%d\n", i);
+			break;
+		}
 	}
-	else if (a > b)
-	{
-		for (int i = b; i > 0; i--)
-			if (a % i == 0 && b % i == 0)
-			{
-				printf("%d\n", i);
-				break;
-			}
-	}
-	else
-		printf("%d\n", a);
 }
