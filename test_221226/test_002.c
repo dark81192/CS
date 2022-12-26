fec63ef500a0c64849fcb6625d02d52f049e88f0
dark81192 <469144759@qq.com>
2022-12-26 19:37:46 +0800

    取两个数的最大公约数

--- test_221226/test_002.c
+++ test_221226/test_002.c
@@ -1,12 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
 
 void commonDivisor()
 {
-	int a = 20, b = 40;
+	int a = 0, b = 0;
+	scanf("%d %d", &a, &b);
 	if (a < b)
 	{
 		for (int i = a; i > 0; i--)
-			if (b % i == 0)
+			if (b % i == 0 && a % i == 0)
 			{
 				printf("%d\n", i);
 				break;
@@ -15,7 +18,7 @@ void commonDivisor()
 	else if (a > b)
 	{
 		for (int i = b; i > 0; i--)
-			if (a % i == 0)
+			if (a % i == 0 && b % i == 0)
 			{
 				printf("%d\n", i);
 				break;
