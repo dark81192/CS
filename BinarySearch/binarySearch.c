e759a7a36bb0198e7744a43877eebcbe5163954d
dark81192 <469144759@qq.com>
2022-12-27 17:32:59 +0800

    please enter the commit message for your changes. Lines starting

--- BinarySearch/binarySearch.c
+++ BinarySearch/binarySearch.c
@@ -2,23 +2,16 @@
 
 #include <stdio.h>
 
-int main()
+int binsearch(int x, int arr[], int len)
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int len = sizeof(arr) / sizeof(arr[0]);
-	int search = 6;	//需要查找的数字
-	int left = 0;	//左下标
-	int right = len - 1;
-	int mid;
-	
 	for (mid = (left + right) / 2; 1; mid = (left + right) / 2)
 	{
 		if (arr[mid] > search)
-			right = mid;
+			right = mid - 1;
 		if (arr[mid] < search)
 			left = mid + 1;
 		if (arr[mid] == search)
-		{ 
+		{
 			printf("%d 的下标是: %d\n", search, mid);
 			break;
 		}
@@ -29,6 +22,19 @@ int main()
 			break;
 		}
 	}
+	return 0;
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	int search = 6;	//需要查找的数字
+	int left = 0;	//左下标
+	int right = len - 1;
+	int mid;
+	
+	
 	
 	return 0;
 }
