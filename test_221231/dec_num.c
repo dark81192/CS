d9be489ae995d6b121790085b3375ee8b5add60b
dark81192 <469144759@qq.com>
2022-12-31 21:33:22 +0800

    递归与函数练习

---
+++ test_221231/dec_num.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void print(unsigned int n)
+{
+	if (n > 9)
+	{
+		print(n / 10);
+	}
+	printf("%d ", n % 10);
+}
+
+void dec_num()
+{
+	unsigned int num;
+	printf(" ‰»Î ˝◊÷£∫");
+	scanf("%u", &num);
+	print(num);
+}
---
+++ test_221231/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include "dec_num.h"
+#include "my_strlen.h"
+
+int main()
+{
+	int option;
+	printf("1.递归打印分解数字\n");
+	printf("2.递归判断字符数组长度\n");
+	scanf("%d", &option);
+	switch (option)
+	{
+	case 1:
+		dec_num();
+		break;
+	case 2:
+		my_strlen();
+		break;
+	default:
+		break;
+	}
+	return 0;
+}
