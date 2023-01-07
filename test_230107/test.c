9fcf9b407f342a0863f8ded153248fc01670dd6a
dark81192 <469144759@qq.com>
2023-01-07 23:27:13 +0800

    循环和分支的练习

---
+++ test_230107/test.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void n_count()
+{
+	int i = 0;
+	int count = 0;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 10 == 9) count++;
+		if (i / 10 == 9) count++;
+	}
+
+	printf("count = %d\n", count);
+}
+
+void sum_d()
+{
+	int i = 0;
+	int flag = 1;
+	float sum = 0.0;
+
+	for (i = 1; i < 101; i++)
+	{
+		sum += flag * 1.0 / i;
+		flag = -flag;
+	}
+
+	printf("sum = %f\n", sum);
+}
+
+void find_max()
+{
+	int arr1[10] = { 0 };
+	int i = 0;
+	
+	printf("输入十位整数空格分割:");
+
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr1[i]);
+	}
+
+	int max = arr1[0];
+
+	for (i = 0; i < 10; i++)
+	{
+		if (max < arr1[i]) max = arr1[i];
+	}
+
+	printf("max = %d \n", max);
+}
+
+int main()
+{
+	int o = 0;
+
+	printf("1.1-100之间9出现的次数\n");
+	printf("2.1/1+1/2+1/3+......+1/100的值\n");
+
+	scanf("%d", &o);
+	
+	switch (o)
+	{
+	case 1:
+		n_count();
+		break;
+
+	case 2:
+		sum_d();
+		break;
+
+	case 3:
+		find_max();
+		break;
+
+	default:
+		printf("error\n");
+		system("pause");
+		break;
+	}
+
+	return 0;
+}
