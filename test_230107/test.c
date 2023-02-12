c67e64c138d194839e1ede07c303d2a955085b7e
dark81192 <469144759@qq.com>
2023-02-12 14:15:14 +0800

    exercise

--- test_230107/test.c
+++ test_230107/test.c
@@ -22,7 +22,7 @@ void sum_d()
 {
 	int i = 0;
 	int flag = 1;
-	float sum = 0.0;
+	double sum = 0.0;
 
 	for (i = 1; i < 101; i++)
 	{
@@ -30,7 +30,7 @@ void sum_d()
 		flag = -flag;
 	}
 
-	printf("sum = %f\n", sum);
+	printf("sum = %.3f\n", sum);
 }
 
 void find_max()
@@ -117,6 +117,89 @@ void ppnumbers()
 	}
 }
 
+void leapYear()
+{
+	int year = 0;
+
+	printf("please enter the year:>");
+	scanf("%d", &year);
+
+	if (year % 4 == 0 && year % 100 != 0)
+		printf("leap year!\n");
+	else if (year % 400 == 0)
+		printf("leap year!\n");
+	else
+		printf("it's not a leap year!\n");
+}
+
+void mulx(int a, int b, int x)
+{
+	if (a > x)
+		return;
+	else if (a == b)
+	{
+		printf("%d * %d = %-2d\n", a, b, a * b);
+		mulx(a + 1, 1, x);
+	}
+	else if (a > b)
+	{
+		printf("%d * %d = %-2d ", a, b, a * b);
+		mulx(a, b + 1, x);
+	}
+}
+
+void mulx_table()
+{
+	int x = 0;
+	int a = 1;
+	int b = 1;
+
+	printf("please enter the number of lines:>");
+	scanf("%d", &x);
+
+	mulx(a, b, x); 
+}
+
+void exchange()
+{
+	int a = 0, b = 0, c = 0;
+
+	printf("please enter the first number:>");
+	scanf("%d", &a);
+	printf("please enter the second number:>");
+	scanf("%d", &b);
+
+	c = a;
+	a = b;
+	b = c;
+
+	printf("first number: %d\nsecond number: %d\n", a, b);
+}
+
+void func_printNum(int num)
+{
+	if (num < 10)
+	{
+		printf("%d\n", num);
+	}
+	else
+	{
+		printf("%d ", num % 10);
+		return func_printNum(num / 10);
+	}
+
+}
+
+void print_num()
+{
+	int num = 0;
+	
+	printf("please enter a number:>");
+	scanf("%d", &num);
+
+	func_printNum(num);
+}
+
 int main()
 {
 	int o = 0;
@@ -127,6 +210,10 @@ int main()
 	printf("4.输出99乘法表\n");
 	printf("5.猜数字游戏\n");
 	printf("6.打印100-200之间的素数\n");
+	printf("7.判断闰年\n");
+	printf("8.交换两个整数\n");
+	printf("9.指定行数的乘法表\n");
+	printf("10.递归打印一个数的每一位\n");
 
 	scanf("%d", &o);
 	
@@ -155,6 +242,22 @@ int main()
 
 	case 6:
 		ppnumbers();
+		break;
+
+	case 7:
+		leapYear();
+		break;
+
+	case 8:
+		exchange();
+		break;
+
+	case 9:
+		mulx_table();
+		break;
+
+	case 10:
+		print_num();
 		break;
 
 	default:
