c39b8d526f01d57cefb0167a3bff2bfb1444d423
dark81192 <469144759@qq.com>
2023-02-22 00:13:18 +0800

    Two-dimensional array

---
+++ test_230220/test_230220.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void px(int arr[], int l)
+{
+	int i = 0;
+	int tmp = 0;
+	int j = 0;
+	
+	for (i = 0; i < l; i++)
+	{
+		printf("arr[%d] = %d ", i, arr[i]);
+	}
+	printf("\n");
+
+	for (i = 0; i < l - 1; i++)
+	{
+		for (j = 0; j < l - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+	for (i = 0; i < l; i++)
+	{
+		printf("arr[%d] = %d ", i, arr[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+	int l = (sizeof(arr)) / sizeof(arr[0]);
+
+	px(arr, l);
+
+	return 0;
+}
+
