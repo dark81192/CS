b6a5d941c3e5fb47bb42c13a8ae67e702124de68
dark81192 <469144759@qq.com>
2023-03-16 00:24:05 +0800

    三子棋游戏

---
+++ test_230311/game.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define ROW 3
+#define COL 3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void InitBoard(char board[ROW][COL], int row, int col);
+
+void DisplayBoard(char board[ROW][COL], int row, int col);
+
+void PlayerMove(char board[ROW][COL], int row, int col);
+
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+int WhoWin(char board[ROW][COL], int row, int col);
---
+++ test_230311/test.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "game.h"
+
+void menu()
+{
+	printf("------------------\n");
+	printf("------1.开始------\n");
+	printf("------0.退出------\n");
+	printf("------------------\n");
+}
+
+void game()
+{
+	//创建二维数组;
+	char board[ROW][COL];
+	//初始化棋盘
+	InitBoard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		//判断输赢
+		int Win = WhoWin(board, ROW, COL);
+
+		if (Win == 1)
+		{
+			printf("恭喜获胜！\n");
+			system("pause");
+			break;
+		}
+		else if (Win == 2)
+		{
+			printf("你输了！\n");
+			system("pause");
+			break;
+		}
+		else if (Win == 0) {
+			printf("平局了！\n");
+			system("pause");
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int select = 0;
+
+	do
+	{
+		system("cls");
+		menu();
+
+		printf("请选择:>");
+		scanf("%d", &select);
+
+		switch (select)
+		{
+		case 0:
+			break;
+
+		case 1:
+			system("cls");
+			game();
+			break;
+
+		default:
+			printf("input error!\n");
+			system("pause");
+			system("cls");
+			break;
+		}
+	} while (select);
+
+	return 0;
+}
