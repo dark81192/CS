82a0cecf92681b4096d4b429d1b2944f2897fa3b
dark81192 <469144759@qq.com>
2024-03-30 22:30:49 +0800

    qsort struct and array

--- exerciseCases/QsortFunisUsed.c
+++ exerciseCases/QsortFunisUsed.c
@@ -1,13 +1,28 @@
 #include "Cases_functions.h"
 #include <stdlib.h>
 
+struct stu
+{
+	char name[20];
+	int age;
+};
 
 int qsort_int(const void* e1, const void* e2)
 {
 	return *(int*)e1 - *(int*)e2;
 }
 
-void print_1(int* arr, int sz)
+int qsort_str_age(const void* e1, const void* e2)
+{
+	return ((struct stu*)e1) -> age - ((struct stu*)e2) -> age;
+}
+
+int qsort_str_name(const void* e1, const void* e2)
+{
+	return strcmp((((struct stu*)e1)->name), (((struct stu*)e2)->name));
+}
+
+void print_int(int* arr, int sz)
 {
 	int i = 0;
 	
@@ -15,22 +30,43 @@ void print_1(int* arr, int sz)
 	{
 		printf("%d ", (*(arr + i)));
 	}
+
+	printf("\n");
 }
 
-struct stu
+void print_str_age(struct stu* arr, int sz)
 {
-	char name[20];
-	int age;
-};
+	int i = 0;
+
+	for (i = 0; i < sz; i++)
+	{
+		printf("qsort age: %d %s\n", (arr + i)->age, (arr + i)->name);
+	}
+}
+
+void print_str_name(struct stu* arr, int sz)
+{
+	int i = 0;
+
+	for (i = 0; i < sz; i++)
+	{
+		printf("qsort name: %d %s\n", (arr + i)->age, (arr + i)->name);
+	}
+}
 
 void QsortFunIsUsed()
 {
 	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
  
-	/*struct stu s[] = { {"zhangsan", 30},{"lisi", 40}, {"wangwu",50} };*/
+	struct stu s[3] = { {"zhangsan", 30},{"lisi", 40}, {"wangwu",50} };
 	int sz = sizeof(arr) / sizeof(arr[0]);
+	int sz_s = sizeof(s) / sizeof(s[0]);
 
 	qsort(arr, sz, sizeof(arr[0]), qsort_int);
+	qsort(s, sz_s, sizeof(s[0]), qsort_str_age);
+	qsort(s, sz_s, sizeof(s[0]), qsort_str_name);
 
-	print_1(arr, sz);
+	print_int(arr, sz);
+	print_str_age(s, sz_s);
+	print_str_name(s, sz_s);
 }
