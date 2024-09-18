9b3cadfa20c13654d8c5614a7c4badc22167e65e
dark81192 <469144759@qq.com>
2024-09-18 22:57:13 +0800

    exercisesCases_myString_strcmp_update

--- exerciseCases/myString.c
+++ exerciseCases/myString.c
@@ -59,7 +59,11 @@ int myStrcmpFunc(const char* str1, const char* str2)
 {
 	assert(str1 && str2);
 
-	while (*str1++ == *str2++ && *str1 != '\0' && *str2 != '\0');
+	while (*str1 == *str2 && *str1 != '\0' && *str2 != '\0')
+	{
+		str1++;
+		str2++;
+	}
 
 	return *str1 - *str2;
 }
